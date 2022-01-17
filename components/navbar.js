09f00eefbc593a002b1cee7113e433a1cc036cf0
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-01-17 17:08:02 +0530

    add

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+
+
+
+    return `<div id="navbar">
+    <a href="/index.html"><div id="home">Home</div><a>
+    <a href="/search.html"><div id="search">Search</div><a>
+    <a href="/news.html"><div id="news">News</div><a>
+    <a href="/subscribe.html"><div id="Subscribe">Subscribe</div><a>
+  </div>`
+}
+
+export default navbar;
